61616c2cea25ee2507cbaa9215786aba0e4211d1
Javier Perez <javiborges95@gmail.com>
2024-10-01 22:31:32 -0600

    Remove noise.png and update Tailwind config
    Deleted the noise.png image and removed related configuration for a custom background image in Tailwind. This simplifies the config and removes the unnecessary file from the project.

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,18 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 const colors = require('tailwindcss/colors');
 const defaultTheme = require('tailwindcss/defaultTheme');
-const fs = require('fs');
-
-const noiseBitmap = fs.readFileSync('./src/assets/noise.png', { encoding: 'base64' });
-const noiseDataUri = 'data:image/png;base64,' + noiseBitmap;
 
 export default {
     content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
     theme: {
         extend: {
-            backgroundImage: {
-                'grid-pattern': `linear-gradient(to bottom, theme('colors.neutral.950 / 0%'), theme('colors.neutral.950 / 100%')), url('${noiseDataUri}')`
-            },
             colors: {
                 neutral: colors.neutral,
                 brandMain: '#1ea8ab',
