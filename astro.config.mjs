2c695a85c24911f4e68b6d1910bd12a347e7cc21
Javier Perez <javiborges95@gmail.com>
2024-10-07 12:12:55 -0600

    Remove React-related dependencies and plugins
    This commit removes all React-related packages, including dependencies and plugins from yarn.lock, package-lock.json, and astro.config.mjs. It should streamline the project by eliminating unnecessary React setup and focus on Tailwind and Netlify configurations.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,10 @@
 import { defineConfig } from 'astro/config';
 import netlify from '@astrojs/netlify';
-import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
     integrations: [
-        react(),
         tailwind({
             applyBaseStyles: false
         })
